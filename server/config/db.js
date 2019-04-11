e0b92d9793a82989eaf3ac585352124086065e57
Tanureet Kaur <tanureetkaur@Tanureets-MacBook-Air.local>
2019-04-11 19:42:54 -0400

    final

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
    //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    "URI" : "mongodb://tanureet:a123456@ds040089.mlab.com:40089/assignment2-tanureet",
     "secret": "SomeSecret"
 }
